7da0149fd5dd79b0e75532c918940974b9cb25e8
Kristian <krisvl5000@gmail.com>
2023-04-23 13:10:58 +0300

    Update Dealer.cs

--- Blackjack/Blackjack/Models/Entities/Dealer.cs
+++ Blackjack/Blackjack/Models/Entities/Dealer.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using Blackjack.Models.Cards;
 
@@ -21,5 +23,10 @@ namespace Blackjack.Models.Entities
         public int Balance { get; set; }
 
         public Deck Deck { get; set; }
+
+        public void Deal()
+        {
+
+        }
     }
 }
