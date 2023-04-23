fe6cbcd379e815d617024ab653c1a2a969ee2f15
Kristian <krisvl5000@gmail.com>
2023-04-23 18:55:07 +0300

    Update IEntity.cs

--- Blackjack/Blackjack/Models/Entities/IEntity.cs
+++ Blackjack/Blackjack/Models/Entities/IEntity.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blackjack.Models.Cards;
 
 namespace Blackjack.Models.Entities
 {
@@ -13,5 +14,7 @@ namespace Blackjack.Models.Entities
         public int Balance { get; set; }
 
         public int Score { get; set; }
+
+        public List<Card> Hand { get; set; }
     }
 }
