2902bd13758d58cd48d6112c0235c44dd60da9bf
Kristian <krisvl5000@gmail.com>
2023-04-23 13:20:52 +0300

    Create Controller.cs

---
+++ Blackjack/Blackjack/Core/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Core
+{
+    public class Controller
+    {
+        public Controller()
+        {
+            
+        }
+
+        public void InitialDealing()
+        {
+
+        }
+
+        public void Hit()
+        {
+
+        }
+
+        public void Stand()
+        {
+
+        }
+
+        public void Split()
+        {
+
+        }
+
+        public void DoubleDown()
+        {
+
+        }
+    }
+}
