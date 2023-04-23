e81053a28536bf7f89c1c17594a2c069e806efdc
Kristian <krisvl5000@gmail.com>
2023-04-23 14:03:44 +0300

    Update Engine.cs

--- Blackjack/Blackjack/Core/Engine.cs
+++ Blackjack/Blackjack/Core/Engine.cs
@@ -10,18 +10,25 @@ namespace Blackjack.Core
 {
     public class Engine
     {
-        private IEntity dealer;
+        private Dealer dealer;
         private IEntity player;
+        private Controller controller;
 
-        public Engine(IEntity dealer, IEntity player)
+        public Engine(Dealer dealer, Player player)
         {
             this.dealer = dealer;
             this.player = player;
+            controller = new Controller(this.dealer, this.player);
         }
 
         public void Run()
         {
+            controller.InitialDealing();
 
+            while (true)
+            {
+                
+            }
         }
     }
 }
