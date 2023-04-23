2e13825bba29c1bb85890c2e9d342a6dcc48d646
Kristian <krisvl5000@gmail.com>
2023-04-23 19:23:07 +0300

    Update Engine.cs

--- Blackjack/Blackjack/Core/Engine.cs
+++ Blackjack/Blackjack/Core/Engine.cs
@@ -23,6 +23,42 @@ namespace Blackjack.Core
 
         public void Run()
         {
+            Console.WriteLine($"OK, let's play some blackjack! How much do you want to bet? Your balance is {player.Balance}.");
+
+            int bet = 0;
+
+            while (true)
+            {
+                bool exceptionCaught = false;
+
+                try
+                {
+                    bet = int.Parse(Console.ReadLine());
+
+                    if (bet > player.Balance)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    exceptionCaught = true;
+                    Console.WriteLine($"Bet value cannot be greater than your balance ({player.Balance}).");
+                }
+                catch (Exception ex)
+                {
+                    exceptionCaught = true;
+                    Console.WriteLine("Please only write whole numbers as bets.");
+                }
+
+                if (!exceptionCaught)
+                {
+                    break;
+                }
+            }
+
+            controller.Bet(bet);
+
             controller.InitialDealing();
 
             while (true)
