df653e0792c20bae810bf5434b6e8f10dcfbb9a0
Kristian <krisvl5000@gmail.com>
2023-04-27 11:24:12 +0300

    Update Form1.Designer.cs

--- Breakout/Breakout/Form1.Designer.cs
+++ Breakout/Breakout/Form1.Designer.cs
@@ -80,27 +80,27 @@
             // player
             // 
             this.player.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.player.Location = new System.Drawing.Point(355, 406);
+            this.player.Location = new System.Drawing.Point(319, 414);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(109, 32);
+            this.player.Size = new System.Drawing.Size(77, 24);
             this.player.TabIndex = 1;
             this.player.TabStop = false;
             // 
             // ball
             // 
             this.ball.BackColor = System.Drawing.Color.Yellow;
-            this.ball.Location = new System.Drawing.Point(394, 301);
+            this.ball.Location = new System.Drawing.Point(349, 356);
             this.ball.Name = "ball";
-            this.ball.Size = new System.Drawing.Size(30, 32);
+            this.ball.Size = new System.Drawing.Size(20, 19);
             this.ball.TabIndex = 1;
             this.ball.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox1.Location = new System.Drawing.Point(46, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(37, 57);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox1.Size = new System.Drawing.Size(77, 26);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Tag = "blocks";
@@ -108,9 +108,9 @@
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox2.Location = new System.Drawing.Point(199, 54);
+            this.pictureBox2.Location = new System.Drawing.Point(178, 57);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox2.Size = new System.Drawing.Size(77, 26);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Tag = "blocks";
@@ -118,9 +118,9 @@
             // pictureBox3
             // 
             this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox3.Location = new System.Drawing.Point(354, 54);
+            this.pictureBox3.Location = new System.Drawing.Point(319, 57);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox3.Size = new System.Drawing.Size(77, 26);
             this.pictureBox3.TabIndex = 1;
             this.pictureBox3.TabStop = false;
             this.pictureBox3.Tag = "blocks";
@@ -128,9 +128,9 @@
             // pictureBox4
             // 
             this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox4.Location = new System.Drawing.Point(502, 54);
+            this.pictureBox4.Location = new System.Drawing.Point(465, 57);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox4.Size = new System.Drawing.Size(77, 26);
             this.pictureBox4.TabIndex = 1;
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Tag = "blocks";
@@ -138,9 +138,9 @@
             // pictureBox5
             // 
             this.pictureBox5.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox5.Location = new System.Drawing.Point(662, 54);
+            this.pictureBox5.Location = new System.Drawing.Point(585, 57);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox5.Size = new System.Drawing.Size(77, 26);
             this.pictureBox5.TabIndex = 1;
             this.pictureBox5.TabStop = false;
             this.pictureBox5.Tag = "blocks";
@@ -148,9 +148,9 @@
             // pictureBox6
             // 
             this.pictureBox6.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox6.Location = new System.Drawing.Point(662, 115);
+            this.pictureBox6.Location = new System.Drawing.Point(585, 118);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox6.Size = new System.Drawing.Size(77, 26);
             this.pictureBox6.TabIndex = 1;
             this.pictureBox6.TabStop = false;
             this.pictureBox6.Tag = "blocks";
@@ -158,9 +158,9 @@
             // pictureBox7
             // 
             this.pictureBox7.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox7.Location = new System.Drawing.Point(502, 115);
+            this.pictureBox7.Location = new System.Drawing.Point(465, 118);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox7.Size = new System.Drawing.Size(77, 26);
             this.pictureBox7.TabIndex = 1;
             this.pictureBox7.TabStop = false;
             this.pictureBox7.Tag = "blocks";
@@ -168,9 +168,9 @@
             // pictureBox8
             // 
             this.pictureBox8.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox8.Location = new System.Drawing.Point(354, 115);
+            this.pictureBox8.Location = new System.Drawing.Point(319, 118);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox8.Size = new System.Drawing.Size(77, 26);
             this.pictureBox8.TabIndex = 1;
             this.pictureBox8.TabStop = false;
             this.pictureBox8.Tag = "blocks";
@@ -178,9 +178,9 @@
             // pictureBox9
             // 
             this.pictureBox9.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox9.Location = new System.Drawing.Point(199, 115);
+            this.pictureBox9.Location = new System.Drawing.Point(178, 118);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox9.Size = new System.Drawing.Size(77, 26);
             this.pictureBox9.TabIndex = 1;
             this.pictureBox9.TabStop = false;
             this.pictureBox9.Tag = "blocks";
@@ -188,9 +188,9 @@
             // pictureBox10
             // 
             this.pictureBox10.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox10.Location = new System.Drawing.Point(46, 115);
+            this.pictureBox10.Location = new System.Drawing.Point(37, 118);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox10.Size = new System.Drawing.Size(77, 26);
             this.pictureBox10.TabIndex = 1;
             this.pictureBox10.TabStop = false;
             this.pictureBox10.Tag = "blocks";
@@ -198,9 +198,9 @@
             // pictureBox11
             // 
             this.pictureBox11.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox11.Location = new System.Drawing.Point(662, 178);
+            this.pictureBox11.Location = new System.Drawing.Point(585, 181);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox11.Size = new System.Drawing.Size(77, 26);
             this.pictureBox11.TabIndex = 1;
             this.pictureBox11.TabStop = false;
             this.pictureBox11.Tag = "blocks";
@@ -208,9 +208,9 @@
             // pictureBox12
             // 
             this.pictureBox12.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox12.Location = new System.Drawing.Point(502, 178);
+            this.pictureBox12.Location = new System.Drawing.Point(465, 181);
             this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox12.Size = new System.Drawing.Size(77, 26);
             this.pictureBox12.TabIndex = 1;
             this.pictureBox12.TabStop = false;
             this.pictureBox12.Tag = "blocks";
@@ -218,9 +218,9 @@
             // pictureBox13
             // 
             this.pictureBox13.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox13.Location = new System.Drawing.Point(354, 178);
+            this.pictureBox13.Location = new System.Drawing.Point(319, 181);
             this.pictureBox13.Name = "pictureBox13";
-            this.pictureBox13.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox13.Size = new System.Drawing.Size(77, 26);
             this.pictureBox13.TabIndex = 1;
             this.pictureBox13.TabStop = false;
             this.pictureBox13.Tag = "blocks";
@@ -228,9 +228,9 @@
             // pictureBox14
             // 
             this.pictureBox14.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox14.Location = new System.Drawing.Point(199, 178);
+            this.pictureBox14.Location = new System.Drawing.Point(37, 181);
             this.pictureBox14.Name = "pictureBox14";
-            this.pictureBox14.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox14.Size = new System.Drawing.Size(77, 26);
             this.pictureBox14.TabIndex = 1;
             this.pictureBox14.TabStop = false;
             this.pictureBox14.Tag = "blocks";
@@ -238,9 +238,9 @@
             // pictureBox15
             // 
             this.pictureBox15.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pictureBox15.Location = new System.Drawing.Point(46, 178);
+            this.pictureBox15.Location = new System.Drawing.Point(178, 181);
             this.pictureBox15.Name = "pictureBox15";
-            this.pictureBox15.Size = new System.Drawing.Size(109, 32);
+            this.pictureBox15.Size = new System.Drawing.Size(77, 26);
             this.pictureBox15.TabIndex = 1;
             this.pictureBox15.TabStop = false;
             this.pictureBox15.Tag = "blocks";
@@ -255,7 +255,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(684, 461);
             this.Controls.Add(this.ball);
             this.Controls.Add(this.pictureBox15);
             this.Controls.Add(this.pictureBox14);
