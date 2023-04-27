ca12cba68cc21551b9780ccfdb6595d56a64133f
Kristian <krisvl5000@gmail.com>
2023-04-27 21:05:53 +0300

    Update Form1.Designer.cs

--- 4 Ways Sprite Movement Project/4 Ways Sprite Movement Project/Form1.Designer.cs
+++ 4 Ways Sprite Movement Project/4 Ways Sprite Movement Project/Form1.Designer.cs
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lblMovement = new System.Windows.Forms.Label();
+            this.movementTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // lblMovement
+            // 
+            this.lblMovement.AutoSize = true;
+            this.lblMovement.BackColor = System.Drawing.Color.Transparent;
+            this.lblMovement.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMovement.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblMovement.Location = new System.Drawing.Point(12, 578);
+            this.lblMovement.Name = "lblMovement";
+            this.lblMovement.Size = new System.Drawing.Size(104, 24);
+            this.lblMovement.TabIndex = 0;
+            this.lblMovement.Text = "Movement:";
+            // 
+            // movementTimer
+            // 
+            this.movementTimer.Enabled = true;
+            this.movementTimer.Interval = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 611);
+            this.Controls.Add(this.lblMovement);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "4 Sprite Animation";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMovement;
+        private System.Windows.Forms.Timer movementTimer;
     }
 }
 
