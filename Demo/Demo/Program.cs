17de453c6ebdae24cb35b73abe0c009f93da1d75
Kristian <krisvl5000@gmail.com>
2023-05-21 15:40:09 +0300

    Update Program.cs

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -6,13 +6,7 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 1, 2, 3, 4, 5 };
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = arr[i] + 2;
-                Console.WriteLine(arr[i]);
-            }
+            
         }
     }
 }
