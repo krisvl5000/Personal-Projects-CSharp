8ae051809a970922c253d5dfb08160f4440d8daa
Kristian <krisvl5000@gmail.com>
2023-05-25 12:40:51 +0300

    Update Program.cs

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -2,11 +2,41 @@
 
 namespace _01._Hello_Softuni
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Solution.IsSubsequence("acb", "ahbgdc"));
+        }
+    }
+
+    public static class Solution
+    {
+        public static bool IsSubsequence(string s, string t)
+        {
+            var sList = s.ToList();
+            var tList = t.ToList();
+
+            var finalList = new List<char>();
+
+            foreach (var t1 in sList)
+            {
+                foreach (var t2 in tList)
+                {
+                    if (t1 == t2)
+                    {
+                        finalList.Add(t1);
+                        break;
+                    }
+                }
+            }
+
+            if (string.Join("", finalList) == s)
+            {
+                return true;    
+            }
+
+            return false;
         }
     }
 }
